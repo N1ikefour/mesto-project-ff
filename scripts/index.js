c458575f479cb81baac6a86d6141ae82c3978a62
N1ke4 <neks2000@bk.ru>
2024-05-10 15:01:15 +0500

    first-try

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+const content = document.querySelector('.content');
+const cardsContainer = content.querySelector('.places__list');
 // @todo: Функция создания карточки
 
-// @todo: Функция удаления карточки
 
+const cardAdd = (cardData, onDelete) => {
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+  cardElement.querySelector('.card__title').textContent = cardData.name;
+  cardElement.querySelector('.card__image').src = cardData.link;
+  cardElement.querySelector('.card__image').alt = cardData.name;
+
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', () => {
+    onDelete(cardElement);
+  })
+  return cardElement;
+  
+}
+// @todo: Функция удаления карточки
+function onDelete (deleteCard) {
+  deleteCard.remove();
+}
 // @todo: Вывести карточки на страницу
+
+function outputInitialCards (cards) {
+  cards.forEach((data) =>{
+    const card = cardAdd(data, onDelete);
+    cardsContainer.append(card)
+  })
+}
+outputInitialCards(initialCards);
